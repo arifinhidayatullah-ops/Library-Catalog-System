465ece089e17608ea8a5da46f19268e781d45262
Arifin Hidayatullah <arifinhidayat899@gmail.com>
2026-04-01 11:17:51 +0700

    update main

--- src/main.java
+++ src/main.java
@@ -1,8 +1,12 @@
 public class main {
     public static void main(String[] args) {
 
+        // test Book
         Book b1 = new Book("001", "Java Basics", "Andi", 2020, true);
-
         b1.display();
+
+        // test Member
+        member m1 = new member("M01", "Arifin", "arifin@email.com");
+        m1.display();
     }
 }
