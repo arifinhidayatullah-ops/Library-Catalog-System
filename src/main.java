e7f890d3cebe277234c97cf04f7759ef29487f12
Arifin Hidayatullah <arifinhidayat899@gmail.com>
2026-04-22 16:54:26 +0700

    update

--- src/main.java
+++ src/main.java
@@ -1,12 +1,25 @@
+import shared.*;
+import student2.BorrowQueue;
+
 public class main {
     public static void main(String[] args) {
 
         // test Book
         Book b1 = new Book("001", "Java Basics", "Andi", 2020, true);
-        b1.display();
 
         // test Member
         member m1 = new member("M01", "Arifin", "arifin@email.com");
+
+        // test br
+        BorrowRecort br1 = new BorrowRecort("M01", "001", "2026-03-10");
+
+        b1.display();
         m1.display();
+        br1.display();
+
+        // simulasi balik buku
+        br1.returnBook("2026-03-15");
+
+        br1.display();
     }
 }
