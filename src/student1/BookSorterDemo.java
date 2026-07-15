a8b728ba8742fcc57997638c94ebc7b2be3f3729
AbrarDanendra <abrar.danendra2007@gmail.com>
2026-07-15 20:18:59 +0700

    update typo

--- src/student1/BookSorterDemo.java
+++ src/student1/BookSorterDemo.java
@@ -6,7 +6,7 @@ public class BookSorterDemo {
     public static void main(String[] args) {
         Book[] books = new Book[] {
             new Book("978-A","Pemrograman Java","Andi Sulistyo",2020, true),
-            new Book("978-B","Struktut Data","Budi Raharjo",2019,true),
+            new Book("978-B","Struktur Data","Budi Raharjo",2019,true),
             new Book("978-C","Basis Data Modern","Andi Sulistyo",2018,true),
             new Book("978-D","Algoritma Modern","Citra Dewi",2021,true),
         };
